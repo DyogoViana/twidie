f8c8167a4813ae28a0f15ceffccac8ad141e5ee0
DyogoViana <dyogo.vianaa@gmail.com>
2021-12-06 16:08:08 -0300

    FAQ : : Collapsible funcionando. falta apenas alinhar o icone de add.

---
+++ js/materialize.js
@@ -0,0 +1,8 @@
+// Initialization Materialize JS.
+
+
+// FAQ - Perguntas Frequentes.
+document.addEventListener('DOMContentLoaded', function () {
+	var collapse = document.querySelectorAll('.collapsible')
+	M.Collapsible.init(collapse)
+});
